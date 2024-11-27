546e9199fbdf2f9694c36c4607fb92498d871118
João Victor da Silva <jv782063@gmail.com>
2024-11-27 20:43:16 -0300

    tries to implement graph graph

--- internal/algorithm/chart.go
+++ internal/algorithm/chart.go
@@ -32,6 +32,25 @@ func (a *Algorithm) ChartHTML() (string, error) {
 		xAxisData[i] = i + 1
 	}
 
+	axisData := make([]int, a.chromosomeSize+1)
+	for i := range a.chromosomeSize + 1 {
+		axisData[i] = i
+	}
+	graphData := make([]int, a.chromosomeSize+1)
+	graphData[0] = 100
+	for i := range a.chromosomeSize {
+		graphData[i+1] = int(a.locations[i].ToNewGene().Distance)
+	}
+	links := make([]map[string]any, a.chromosomeSize+1)
+	links[0] = map[string]any{"source": a.startingPoint.ID, "target": a.locations[0].ID}
+	for i := range a.chromosomeSize {
+		if i == a.chromosomeSize {
+			links[i+1] = map[string]any{"source": a.locations[i].ID, "target": a.startingPoint.ID}
+		} else {
+			links[i+1] = map[string]any{"source": a.locations[i].ID, "target": a.locations[i].ID}
+		}
+	}
+
 	fitnessOptions := map[string]any{
 		"legend": map[string]any{},
 		"title":  map[string]any{"text": "Convergence - Fitness"},
@@ -120,25 +139,59 @@ func (a *Algorithm) ChartHTML() (string, error) {
 		},
 	}
 
-	return renderTemplate(fitnessOptions, distanceOptions)
+	distanceGraphOptions := map[string]any{
+		"legend": map[string]any{},
+		"title":  map[string]any{"text": "Graph - Distance"},
+		"yAxis":  []map[string]any{{"type": "value"}},
+		"xAxis": map[string]any{
+			"type":        "category",
+			"boundaryGap": false,
+			"data":        axisData,
+		},
+		"tooltip": map[string]any{
+			"show":    true,
+			"trigger": "axis",
+		},
+		"series": []map[string]any{
+			{
+				"type":             "graph",
+				"layout":           "none",
+				"coordinateSystem": "cartesian2d",
+				"symbolSize":       40,
+				"label": map[string]any{
+					"show": true,
+				},
+				"edgeSymbol":     []string{"circle", "arrow"},
+				"edgeSymbolSize": []int{4, 10},
+				"data":           graphData,
+				"links":          links,
+			},
+		},
+	}
+
+	return renderTemplate(fitnessOptions, distanceOptions, distanceGraphOptions)
 }
 
-func renderTemplate(fitnessOptions, distanceOptions any) (string, error) {
+func renderTemplate(fitnessOptions, distanceOptions, distanceGraphOptions any) (string, error) {
 	tmpl, err := template.New("chart").Parse(`
 		<!-- <script src="https://go-echarts.github.io/go-echarts-assets/assets/echarts.min.js"></script> -->
 			<div class="item" id="fitnessChart" style="width:900px;height:500px;"></div>
 			<div class="item" id="distanceChart" style="width:900px;height:500px;"></div>
+			<div class="item" id="distanceGraph" style="width:900px;height:500px;"></div>
 		<script type="text/javascript">
 		"use strict";
 			(function() {
 				let fitnessChart = echarts.init(document.getElementById('fitnessChart'), "dark", { renderer: "canvas" });
 				let distanceChart = echarts.init(document.getElementById('distanceChart'), "dark", { renderer: "canvas" });
+				let distanceGraph = echarts.init(document.getElementById('distanceGraph'), "dark", { renderer: "canvas" });
 
 				let fitnessOption = {{.FitnessOption}};
 				let distanceOption = {{.DistanceOption}};
+				let distanceGraphOption = {{.DistanceGraphOption}};
 
 				fitnessChart.setOption(fitnessOption);
 				distanceChart.setOption(distanceOption);
+				distanceGraph.setOption(distanceGraphOption);
 			})();
 		</script>
 		<style>
@@ -152,8 +205,9 @@ func renderTemplate(fitnessOptions, distanceOptions any) (string, error) {
 
 	var tpl bytes.Buffer
 	data := map[string]any{
-		"FitnessOption":  fitnessOptions,
-		"DistanceOption": distanceOptions,
+		"FitnessOption":       fitnessOptions,
+		"DistanceOption":      distanceOptions,
+		"DistanceGraphOption": distanceGraphOptions,
 	}
 	err = tmpl.Execute(&tpl, data)
 	if err != nil {
