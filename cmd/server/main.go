f0005725b733256f350e5a0ecae77df844ee5819
João Victor da Silva <jv782063@gmail.com>
2024-11-27 21:27:49 -0300

    mocked endpoint data values

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -57,6 +57,43 @@ func main() {
 		json.NewEncoder(w).Encode(api.AlgorithmResponseToApiResponse(algorithmRes, chartsHTML, start))
 	})
 
+	mainMux.HandleFunc("/get-route-test-mock", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		var requestData api.LocationRequest
+
+		if err := json.NewDecoder(r.Body).Decode(&requestData); err != nil {
+			http.Error(w, "failed to decode request body", http.StatusBadRequest)
+			return
+		}
+
+		if err := requestData.Validate(); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		startingPoint, coreLocations, err := requestData.ToCoreLocation()
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error converting to core locations: %v", err), http.StatusBadRequest)
+			return
+		}
+		config := algorithm.NewConfig(requestData.MaxPopulation, requestData.MaxGenerations, requestData.Elitism, requestData.MutationRate)
+		distanceCalculator := distance.NewInBatchCalculator()
+		algorithmInstance := algorithm.NewAlgorithm(config, startingPoint, coreLocations, distanceCalculator)
+
+		start := time.Now()
+		algorithmRes := algorithmInstance.Run()
+
+		go algorithmInstance.RenderChart()
+
+		chartsHTML, err := algorithmInstance.ChartHTML()
+		if err != nil {
+			chartsHTML = "Error when making charts"
+		}
+
+		w.WriteHeader(200)
+		json.NewEncoder(w).Encode(api.AlgorithmResponseToApiResponse(algorithmRes, chartsHTML, start))
+	})
+
 	handler := cors.Default().Handler(mainMux)
 
 	srv := &http.Server{
